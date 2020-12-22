da09a5699c8961ee29b57aa9eb723ccddb917c73
nikito96 <nikinikolitsov9655@gmail.com>
2020-12-22 18:03:33 +0200

    code push

---
+++ src/test/java/com/nikito/picshare/PicshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikito.picshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
